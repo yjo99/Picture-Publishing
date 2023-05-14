5aaf5134677cc52f0e87708b400091f2c629fb8f
yousefeldahshan6 <yousefeldahshan6@gmail.com>
2023-05-14 12:31:26 +0300

    project initializer

---
+++ src/main/java/com/jo/picPublising/PicPublishingApplication.java
@@ -0,0 +1,13 @@
+package com.jo.picPublising;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicPublishingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicPublishingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jo/picPublising/PicPublishingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jo.picPublising;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicPublishingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
